627c04ec332b60ac2e3b689dba94d2ef52f1fa01
KevinLopez13 <enanodeoz@gmail.com>
2023-08-24 12:14:48 -0600

    [UPDATE] 230824 Version with blog section and Django administration for admins.

--- devixaya/ixaya/models.py
+++ devixaya/ixaya/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blogpost(models.Model):
+    title = models.CharField(max_length=200)
+    resume = models.CharField(max_length=60, null=True)
+    content = models.TextField(max_length=3000, null=True)
+    author = models.CharField(max_length=100, null=True)
+    image = models.ImageField(upload_to='img/', blank=True, null=True)
+
+
+class Patrocinador(models.Model):
+    name = models.CharField(max_length=15)
+    description = models.TextField(max_length=200)
--- devixaya/ixaya/urls.py
+++ devixaya/ixaya/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.inicio, name='inicio' ),
     path('inicio', views.inicio, name='inicio' ),
     path('blog', views.blog, name='blog' ),
+    path('blogpost/<post>', views.blogpost ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
